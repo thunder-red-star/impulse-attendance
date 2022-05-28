742fe5f7931747d3271d4a14eccf7f9dfd70d65d
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-28 09:48:15 -0400

    init

--- init.js
+++ init.js
@@ -1,5 +1,19 @@
 const fs = require('fs');
 const path = require('path')
 
-fs.writeFileSync(path.join(__dirname, 'data/attendance.json'), JSON.stringify({}));
-fs.writeFileSync(path.join(__dirname, 'data/users.json'), JSON.stringify([]));
+try {
+	fs.mkdirSync(path.join(__dirname, 'data'));
+} catch (e) {
+	if (e.code !== 'EEXIST') {
+		throw e;
+	}
+}
+
+try {
+	fs.writeFileSync(path.join(__dirname, 'data/attendance.json'), JSON.stringify({}));
+	fs.writeFileSync(path.join(__dirname, 'data/users.json'), JSON.stringify([]));
+} catch (e) {
+	if (e.code !== 'EEXIST') {
+		throw e;
+	}
+}
